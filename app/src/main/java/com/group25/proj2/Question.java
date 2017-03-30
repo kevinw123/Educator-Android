8d6c130a1237c2ef88f68523c7e5d08841a95627
ginayao1995 <ginayao1995@gmail.com>
2017-03-30 11:58:07 -0700

    Add switching to questions activity

---
+++ app/src/main/java/com/group25/proj2/Question.java
@@ -0,0 +1,48 @@
+package com.group25.proj2;
+
+/**
+ * Created by Gina on 3/23/2017.
+ */
+
+public class Question {
+    String question;
+    String answerA;
+    String answerB;
+    String answerC;
+    String answerD;
+    String correctChoice;
+
+    public Question(String question, String answerA, String answerB, String answerC, String answerD, String correctChoice){
+        this.question = question;
+        this.answerA = answerA;
+        this.answerB = answerB;
+        this.answerC = answerC;
+        this.answerD = answerD;
+        this.correctChoice = correctChoice;
+    }
+
+    public String getCorrectChoice() {
+        return correctChoice;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswerA() {
+        return answerA;
+    }
+
+    public String getAnswerB() {
+        return answerB;
+    }
+
+    public String getAnswerC() {
+        return answerC;
+    }
+
+    public String getAnswerD() {
+        return answerD;
+    }
+
+}
